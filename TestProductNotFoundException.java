b39112dbf9be1b39cca6264a47bfd0e3b0163692
matthew-paul <matthew.paul.mail@gmail.com>
2019-04-08 21:42:04 -0500

    Specification Testing
    Tested for increasing item count and balance when product is added.

--- TestProductNotFoundException.java
+++ TestProductNotFoundException.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -9,6 +10,21 @@ public class TestProductNotFoundException {
 		ShoppingCart s = new ShoppingCart();
 		Product p = new Product("Banana", 2.99);
 		
+		// When a new product is added, the number of items must be incremented
+		int size = s.getItemCount();
+		Product apple = new Product("Apple", 1.99);
+		s.addItem(apple);
+		assertEquals(s.getItemCount(), size+1);
+		
+		// When a new product is added, the new balance must be the sum of the previous balance plus the cost of the new product
+		Product orange = new Product("Orange", 0.99);
+		double bal = s.getBalance();
+		s.addItem(orange);
+		if(s.getBalance() != bal+orange.getPrice())
+			fail();
+		
+		
+		// When a product not in the cart is removed, a ProductNotFoundException must be thrown
 		try {
 			s.removeItem(p);
 			fail();
